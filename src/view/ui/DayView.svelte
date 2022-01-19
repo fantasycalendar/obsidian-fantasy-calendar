b4fc4b715998e6611932322e354baf1820228685
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2022-01-18 21:34:48 -0500

    fixed dayview

--- src/view/ui/DayView.svelte
+++ src/view/ui/DayView.svelte
@@ -17,9 +17,10 @@
 
     $: currentDate = calendar.viewedDate;
     $: date = calendar.viewing;
+    $: day = calendar.getDayForDate(date);
     $: dayNumber = calendar.dayNumberForDate(date);
-    $: events = /* calendar.getEventsOnDate(calendar.viewing) */ [];
-    $: moons = /* calendar.getMoonsForDate(calendar.viewing); */ [];
+    $: events = day.events;
+    $: moons = day.moons;
     $: categories = calendar.object.categories;
 
     let displayMoons: boolean;
@@ -29,8 +30,7 @@
     calendar.on("day-update", () => {
         date = calendar.viewing;
         currentDate = calendar.viewedDate;
-        /* events = calendar.getEventsOnDate(calendar.viewing); */
-        /* moons = calendar.getMoonsForDate(calendar.viewing); */
+        day = calendar.getDayForDate(date);
     });
 
     const dispatch = createEventDispatcher();
