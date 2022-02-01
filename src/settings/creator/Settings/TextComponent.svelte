9d798296a158bcbb5898438e0143c2358cf38e59
valentine195 <38669521+valentine195@users.noreply.github.com>
2022-02-01 11:19:01 -0500

    add warning to generic text component

--- src/settings/creator/Settings/TextComponent.svelte
+++ src/settings/creator/Settings/TextComponent.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+    import { warning } from "../Utilities/utils";
     import { createEventDispatcher } from "svelte";
 
     const dispatch = createEventDispatcher();
 
     export let value: string;
     export let name: string;
+    export let warn: boolean = false;
 
     export let desc = "";
 
@@ -13,7 +15,14 @@
 
 <div class="setting-item">
     <div class="setting-item-info">
-        <div class="setting-item-name">{name}</div>
+        <div class="setting-item-name">
+            <div class="name-container">
+                {#if warn}
+                    <div use:warning />
+                {/if}
+                {name}
+            </div>
+        </div>
         <div class="setting-item-description">{desc}</div>
     </div>
     <div class="setting-item-control">
@@ -21,6 +30,7 @@
             type="text"
             spellcheck="false"
             {placeholder}
+            class:warn
             bind:value
             on:blur={() => {
                 dispatch("blur", value);
@@ -28,3 +38,14 @@
         />
     </div>
 </div>
+
+<style>
+    .name-container {
+        display: flex;
+        align-items: center;
+        gap: 0.25rem;
+    }
+    input.warn {
+        border-color: var(--text-error);
+    }
+</style>
