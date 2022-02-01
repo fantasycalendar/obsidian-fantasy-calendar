681bd3258dd938dd892fba1173a1817e11660e17
valentine195 <38669521+valentine195@users.noreply.github.com>
2022-02-01 08:35:45 -0500

    typings update

---
+++ src/@types/event.d.ts
@@ -0,0 +1,28 @@
+export interface Event {
+    name: string;
+    description: string;
+    date: {
+        month: number;
+        day: number;
+        year: number;
+    };
+    end?: {
+        month: number;
+        day: number;
+        year: number;
+    };
+    id: string;
+    note: string;
+    category: string;
+    timestamp?: number;
+}
+
+export interface ColorEvent extends Event {
+    color: string;
+}
+
+export interface EventCategory {
+    name: string;
+    color: string;
+    id: string;
+}
