77616284610d1838cbfed79af67a7b4080f58921
valentine195 <38669521+valentine195@users.noreply.github.com>
2022-02-07 13:38:27 -0500

    slice events to load faster

--- src/settings/creator/Containers/EventContainer.svelte
+++ src/settings/creator/Containers/EventContainer.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-    import { createEventDispatcher } from "svelte";
-
     import type { Event, Calendar } from "src/@types";
     import { dateString } from "src/utils/functions";
 
@@ -10,27 +8,26 @@
     import type FantasyCalendar from "src/main";
     import { CreateEventModal } from "src/settings/modals/event";
     import Details from "../Utilities/Details.svelte";
+    import ButtonComponent from "../Settings/ButtonComponent.svelte";
 
     export let calendar: Calendar;
     export let plugin: FantasyCalendar;
-    $: events = calendar.events;
+    let sliced = 1;
+    $: sorted = calendar.events.sort((a, b) => {
+        if (a.date.year != b.date.year) {
+            return a.date.year - b.date.year;
+        }
+        if (a.date.month != b.date.month) {
+            return a.date.month - b.date.month;
+        }
+        return a.date.day - b.date.day;
+    });
+    $: events = sorted.slice(0, 25 * sliced);
     $: months = calendar.static.months;
 
     const deleteEvent = (item: Event) => {
         events = events.filter((event) => event.id !== item.id);
     };
-
-    $: {
-        events.sort((a, b) => {
-            if (a.date.year != b.date.year) {
-                return a.date.year - b.date.year;
-            }
-            if (a.date.month != b.date.month) {
-                return a.date.month - b.date.month;
-            }
-            return a.date.day - b.date.day;
-        });
-    }
     const getCategory = (category: string) => {
         return calendar.categories.find(({ id }) => id == category);
     };
@@ -54,11 +51,12 @@
 </script>
 
 <Details name={"Events"}>
-    <AddNew on:click={() => add()} />
-
     {#if !events.length}
+        <AddNew on:click={() => add()} />
         <NoExistingItems message={"Create a new event to see it here."} />
     {:else}
+        <ButtonComponent name="Delete All Events" icon="trash" />
+        <AddNew on:click={() => add()} />
         <div class="existing-items">
             {#each events as event}
                 <EventInstance
@@ -70,6 +68,7 @@
                 />
             {/each}
         </div>
+        <div class="more" on:click={() => sliced++}>Load More Events...</div>
     {/if}
 </Details>
 
